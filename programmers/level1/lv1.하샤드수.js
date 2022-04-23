2a3daf44d74d843be943c8452589e974a8126e99
GIO <wnsgyo@gmail.com>
2022-04-23 17:36:14 +0900

    mv directory

---
+++ programmers/level1/lv1.하샤드수.js
@@ -0,0 +1,30 @@
+function solution(x) {
+  let sum = 0;
+  let temp = x;
+
+  while (temp > 0) {
+    sum += temp % 10;
+    temp = Math.floor(temp / 10);
+  }
+
+  if (x % sum === 0) {
+    return true;
+  }
+
+  return false;
+}
+
+function solution2(x) {
+  let sum = x
+    .toString()
+    .split("")
+    .reduce((pre, cur) => (pre += +cur), 0);
+
+  if (x % sum === 0) {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(solution(12));
