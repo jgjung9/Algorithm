ece1af3bc7883eb7ccba86fb20bb8b824f909dec
Gio <wnsgyo@gmail.com>
2022-04-28 06:37:00 +0900

    lv2 행렬곱셈

---
+++ programmers/level2/lv2.행렬의곱셈.js
@@ -0,0 +1,16 @@
+function solution(arr1, arr2) {
+    let answer = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        const row = [];
+        for (let j = 0; j < arr2[0].length; j++) {
+            let aij = 0;
+            for (let k = 0; k < arr2.length; k++) {
+                aij += arr1[i][k] * arr2[k][j];
+            }
+            row.push(aij);
+        }
+        answer.push(row);
+    }
+    return answer;
+}
