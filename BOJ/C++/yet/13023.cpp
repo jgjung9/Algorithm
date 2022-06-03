9a4b77adf4d6c6d7afb547d27e22eab79a30282c
Gio <wnsgyo9@gmail.com>
2022-06-04 03:38:58 +0900

    yet

--- BOJ/C++/yet/13023.cpp
+++ BOJ/C++/yet/13023.cpp
@@ -3,27 +3,3 @@
  *
  * solution:
  */
-#include <iostream>
-#include <vector>
-using namespace std;
-
-vector<int> v[2000];
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n, m;
-    cin >> n >> m;
-    for (int i = 0; i < m; i++)
-    {
-        int from, to;
-        cin >> from >> to;
-        v[from].push_back(to);
-        v[to].push_back(from);
-    }
-
-    cout << 0;
-
-    return 0;
-}
