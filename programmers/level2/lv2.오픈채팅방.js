5be12d136237de19f4cab44f6d5d6cfe9d5e2c85
Gio <wnsgyo9@gmail.com>
2022-06-24 17:09:15 +0900

    lv2 오픈채팅방

---
+++ programmers/level2/lv2.오픈채팅방.js
@@ -0,0 +1,25 @@
+function solution(record) {
+  var answer = [];
+  const ids = new Map();
+  record.forEach((r) => {
+    const [command, id, name] = r.split(' ');
+    if (command === 'Enter' || command === 'Change') ids.set(id, name);
+  });
+
+  record.forEach((r) => {
+    const [command, id, _] = r.split(' ');
+    if (command === 'Enter') answer.push(`${ids.get(id)}님이 들어왔습니다.`);
+    else if (command === 'Leave') answer.push(`${ids.get(id)}님이 나갔습니다.`);
+  });
+
+  console.log(answer);
+  return answer;
+}
+
+solution([
+  'Enter uid1234 Muzi',
+  'Enter uid4567 Prodo',
+  'Leave uid1234',
+  'Enter uid1234 Prodo',
+  'Change uid4567 Ryan',
+]);
