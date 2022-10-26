09baf196b01eff72d48b0e0708c84ae81702ec72
gio <wnsgyo9@gmail.com>
2022-10-26 22:25:07 +0900

    exercise 1

---
+++ codingtestwithcpp/exercies/1.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+template <typename T>
+class dynamic_array<T>
+{
+private:
+    T *data;
+    size_t n;
+
+public:
+    dynamic_array(int n)
+    {
+        this->n = n;
+        data = new T[n];
+    }
+
+    dynamic_array(const dynamic_array<T> &other)
+    {
+        n = other.size();
+        data = new T[n];
+
+        for (int i = 0; i < n; i++)
+            data[i] = other[i];
+    }
+
+    ~dynamic_array()
+    {
+        delete[] data;
+    }
+
+    size_t size() const { return n; }
+    T *begin() { return data; }
+    T *end() { return data + n; }
+    const T *begin() { return data; }
+    const T *end() { return data + n; }
+
+    T &operator[](int index)
+    {
+        return data[index];
+    }
+
+    const T &operator[](int index) const
+    {
+        return data[index];
+    }
+
+    T &at(int index)
+    {
+        if (index < n)
+            return data[index];
+        throw "Index out of range";
+    }
+
+    friend dynamic_array<T> operator+(const dynamic_array<T> &arr1, dynamic_array<T> &arr2)
+    {
+        dynamic_array<T> result(arr1.size() + arr2.size());
+        std::copy(arr1.begin(), arr.end(), result.begin());
+        std::copy(arr2.begin(), arr.end(), result.begin() + arr1.size());
+
+        return result;
+    }
+
+    std::string to_string(const std::string &sep = ", ")
+    {
+        if (n == 0)
+            return "";
+
+        std::ostringstream os;
+        os << data[0];
+
+        for (int i = 1; i < n; i++)
+            os << sep << data[i];
+
+        return os.str();
+    }
+};
+
+struct student
+{
+    std::string name;
+    int standard;
+};
+
+std::ostream &operator<<(std::ostream &os, const student &s)
+{
+    return (os << "[" << s.name << ", " << s.standard << "]");
+}
+
+int main()
+{
+    int nStudents;
+    std::cout << "1반 학생 수를 입력하시오: ";
+    std::cin >> nStudnets;
+
+    dynamic_array<student> class1(nStudents);
+    for (int i = 0; i < nStudents; i++)
+    {
+        std::string name;
+        int standard;
+        std::cout << i + 1 << "번째 학생 이름과 나이를 입력하세요: ";
+        std::cin >> name >> standard;
+        class1[1] = student{name, standard};
+    }
+
+    try
+    {
+        class.at(nStudents) = student{"John", 8};
+    }
+    catch ()
+    {
+        std::cout << "예외 발생!" << std::endl;
+    }
+
+    auto class2 = class1;
+    std::cout << "1반을 복사하여 2반 생성: " << class.to_string() << std::endl;
+
+    auto class3 = class1 + class2;
+    std::cout << "1반과 2반을 합쳐 3반 생성: " << class3.to_string() << std::endl;
+
+    return 0;
+}
