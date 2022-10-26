b7c7b4d6e8922ff4c25ef4927f2235dabb387670
gio <wnsgyo9@gmail.com>
2022-10-26 22:46:38 +0900

    exercise 1

--- codingtestwithcpp/exercies/1.cpp
+++ codingtestwithcpp/exercies/1.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 
 template <typename T>
-class dynamic_array<T>
+class dynamic_array
 {
 private:
     T *data;
@@ -32,9 +32,9 @@ public:
 
     size_t size() const { return n; }
     T *begin() { return data; }
+    const T *begin() const { return data; }
     T *end() { return data + n; }
-    const T *begin() { return data; }
-    const T *end() { return data + n; }
+    const T *end() const { return data + n; }
 
     T &operator[](int index)
     {
@@ -50,14 +50,14 @@ public:
     {
         if (index < n)
             return data[index];
-        throw "Index out of range";
+        throw std::out_of_range("Index out of range");
     }
 
     friend dynamic_array<T> operator+(const dynamic_array<T> &arr1, dynamic_array<T> &arr2)
     {
         dynamic_array<T> result(arr1.size() + arr2.size());
-        std::copy(arr1.begin(), arr.end(), result.begin());
-        std::copy(arr2.begin(), arr.end(), result.begin() + arr1.size());
+        std::copy(arr1.begin(), arr1.end(), result.begin());
+        std::copy(arr2.begin(), arr2.end(), result.begin() + arr1.size());
 
         return result;
     }
@@ -92,7 +92,7 @@ int main()
 {
     int nStudents;
     std::cout << "1반 학생 수를 입력하시오: ";
-    std::cin >> nStudnets;
+    std::cin >> nStudents;
 
     dynamic_array<student> class1(nStudents);
     for (int i = 0; i < nStudents; i++)
@@ -101,20 +101,20 @@ int main()
         int standard;
         std::cout << i + 1 << "번째 학생 이름과 나이를 입력하세요: ";
         std::cin >> name >> standard;
-        class1[1] = student{name, standard};
+        class1[i] = student{name, standard};
     }
 
     try
     {
-        class.at(nStudents) = student{"John", 8};
+        class1.at(nStudents) = student{"John", 8};
     }
-    catch ()
+    catch (const std::out_of_range &e)
     {
-        std::cout << "예외 발생!" << std::endl;
+        std::cerr << e.what() << std::endl;
     }
 
     auto class2 = class1;
-    std::cout << "1반을 복사하여 2반 생성: " << class.to_string() << std::endl;
+    std::cout << "1반을 복사하여 2반 생성: " << class1.to_string() << std::endl;
 
     auto class3 = class1 + class2;
     std::cout << "1반과 2반을 합쳐 3반 생성: " << class3.to_string() << std::endl;
